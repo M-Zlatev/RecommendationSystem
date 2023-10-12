e00f0ccae20502e91956e556d9e14af38820209c
Mariyan Zlatev <mariyan.zlatev.bg@gmail.com>
2023-10-13 00:16:27 +0300

    Add MediatR and FluentValidation.DependencyInjectionExtensions

---
+++ src/RS.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿namespace RS.Application;
+
+#region Usings
+using System.Reflection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using FluentValidation;
+#endregion
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        Assembly assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblies(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
+
